21dfea9725f49cdd6caa1426af161b1b36330668
TABISH <tabishjaved@hotmail.com>
2023-01-23 15:14:00 +0530

    fetching posts from DB

--- routes/blog.js
+++ routes/blog.js
@@ -7,8 +7,13 @@ router.get("/", function (request, response) {
     response.redirect("/posts")
 })
 
-router.get("/posts", function (request, response) {
-    response.render("posts-list")
+router.get("/posts", async function (request, response) {
+    const query = `
+        SELECT posts.*, authors.name AS author_name FROM posts
+        INNER JOIN authors ON posts.author_id = authors.id`
+    const [posts] = await db.query(query)
+    console.log(posts)
+    response.render("posts-list", { posts: posts })
 })
 
 router.get("/new-post", async function (request, response) {
